d9dbc8a3978f4f99c0d1721638e352a711634dc7
zhangqian_g <zq670100705@126.com>
2018-09-05 11:39:21 +0800

    first

---
+++ src/main/java/com/offcn/bean/Person.java
@@ -0,0 +1,9 @@
+package com.offcn.bean;
+
+public class Person {
+	public static void main(String[] args) {
+		System.out.println("============");
+		System.out.println("===============");	
+		System.out.println("----");
+		}
+}
