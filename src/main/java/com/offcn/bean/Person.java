4988fdbbf494a590620173cfe3042da063204842
zhangqian_g <zq670100705@126.com>
2018-09-05 14:05:20 +0800

    change info

--- src/main/java/com/offcn/bean/Person.java
+++ src/main/java/com/offcn/bean/Person.java
@@ -1,10 +1,26 @@
 package com.offcn.bean;
 
 public class Person {
-	public static void main(String[] args) {
-		System.out.println("============");
-		System.out.println("===============");	
-		System.out.println("----");
-		System.out.println("00000");
-		}
+	private Integer id;
+	private String name;
+	private boolean sex;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isSex() {
+		return sex;
+	}
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+	
 }
